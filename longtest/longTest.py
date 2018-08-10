7ef414f653cd3796fed208ebb1bfcf1125c5f67f
Yanhua Gu <yanhua.gu@YanhuaGus-MacBook-Pro.local>
2018-08-10 15:56:14 +0800

    ok

--- longtest/longTest.py
+++ longtest/longTest.py
@@ -126,76 +126,76 @@ class longTest(unittest.TestCase):
 
 
 
-    # def testBuild(self):
-    #
-    #     starttime = datetime.now()
-    #
-    #     url = longTest.base_url + "/cubes/b/segments/build"
-    #     payload = '''{"buildType": "BUILD", "startTime": '''+str(time.time()*1000-648911117000)+''', "endTime": '''+str(time.time()*1000-648675779000)+''', "mpValues": "","project": "test256to310"}
-    #     '''
-    #     status_code = 0
-    #     try_time = 1
-    #     while status_code != 200 and try_time <= 3:
-    #         print 'Submit build job, try_time = ' + str(try_time)
-    #         try:
-    #             response = requests.request("PUT", url, data=payload, headers=longTest.headers)
-    #             status_code = response.status_code
-    #         except:
-    #             status_code = 0
-    #             pass
-    #         if status_code != 200:
-    #             time.sleep(60)
-    #             try_time += 1
-    #
-    #     self.assertEqual(status_code, 200, 'Build job submitted failed.')
-    #
-    #     if status_code == 200:
-    #         print 'Build job is submitted...'
-    #         job_response = json.loads(response.text)
-    #         job_uuid = job_response['data']['uuid']
-    #         job_url = longTest.base_url + "/jobs/" + job_uuid
-    #         job_response = requests.request("GET", job_url, headers=longTest.headers)
-    #
-    #         self.assertEqual(job_response.status_code, 200, 'Build job information fetched failed.')
-    #
-    #         job_info = json.loads(job_response.text)
-    #         job_status = job_info['data']['job_status']
-    #         try_time = 1
-    #         total_try_time = 6000
-    #         print 'Run mode: ' + str(RUN_MODE)
-    #         if RUN_MODE == '1':
-    #             total_try_time = 5
-    #         while job_status in ('RUNNING', 'PENDING') and try_time <= total_try_time:
-    #             print 'Wait for job complete, try_time = ' + str(try_time)
-    #             try:
-    #                 job_response = requests.request("GET", job_url, headers=longTest.headers)
-    #                 job_info = json.loads(job_response.text)
-    #                 job_status = job_info['data']['job_status']
-    #             except:
-    #                 job_status = 'UNKNOWN'
-    #                 pass
-    #             if job_status in ('RUNNING', 'PENDING', 'UNKNOWN'):
-    #                 time.sleep(60)
-    #                 try_time += 1
-    #
-    #         if RUN_MODE == '1':
-    #             while job_status in ('RUNNING'):
-    #                 job_response = requests.request("GET", job_url, headers=longTest.headers)
-    #                 job_info = json.loads(job_response.text)
-    #                 job_status = job_info['data']['job_status']
-    #                 killYarnApplications()
-    #             self.assertEquals(job_status, 'ERROR', 'Build cube failed, job status is ' + job_status)
-    #         else:
-    #             self.assertEquals(job_status, 'FINISHED', 'Build cube failed, job status is ' + job_status)
-    #         print 'Job complete.'
-    #         timeend = datetime.now()
-    #         dtime = (timeend - starttime).seconds
-    #         sql = '''
-    #         INSERT INTO `longtest`.`result`(`id`, `name`, `request`, `respons`, `starttime`, `endtime`, `status`, `date`,`D_time`) VALUES (null, '{namepro}', '', '', '{timestart}', '{timeend}', '{status}','{timeend}', '{D_time}');
-    #         '''
-    #         sql = sql.format(namepro="testQuery1", request1=str(response.url), respons1=str(response.text),
-    #                          timestart=starttime, timeend=timeend, D_time=dtime, status=str(response.status_code))
-    #
+    def testBuild(self):
+
+        starttime = datetime.now()
+
+        url = longTest.base_url + "/cubes/b/segments/build"
+        payload = '''{"buildType": "BUILD", "startTime": '''+str(time.time()*1000-648911117000)+''', "endTime": '''+str(time.time()*1000-648675779000)+''', "mpValues": "","project": "test256to310"}
+        '''
+        status_code = 0
+        try_time = 1
+        while status_code != 200 and try_time <= 3:
+            print 'Submit build job, try_time = ' + str(try_time)
+            try:
+                response = requests.request("PUT", url, data=payload, headers=longTest.headers)
+                status_code = response.status_code
+            except:
+                status_code = 0
+                pass
+            if status_code != 200:
+                time.sleep(60)
+                try_time += 1
+
+        self.assertEqual(status_code, 200, 'Build job submitted failed.')
+
+        if status_code == 200:
+            print 'Build job is submitted...'
+            job_response = json.loads(response.text)
+            job_uuid = job_response['data']['uuid']
+            job_url = longTest.base_url + "/jobs/" + job_uuid
+            job_response = requests.request("GET", job_url, headers=longTest.headers)
+
+            self.assertEqual(job_response.status_code, 200, 'Build job information fetched failed.')
+
+            job_info = json.loads(job_response.text)
+            job_status = job_info['data']['job_status']
+            try_time = 1
+            total_try_time = 6000
+            print 'Run mode: ' + str(RUN_MODE)
+            if RUN_MODE == '1':
+                total_try_time = 5
+            while job_status in ('RUNNING', 'PENDING') and try_time <= total_try_time:
+                print 'Wait for job complete, try_time = ' + str(try_time)
+                try:
+                    job_response = requests.request("GET", job_url, headers=longTest.headers)
+                    job_info = json.loads(job_response.text)
+                    job_status = job_info['data']['job_status']
+                except:
+                    job_status = 'UNKNOWN'
+                    pass
+                if job_status in ('RUNNING', 'PENDING', 'UNKNOWN'):
+                    time.sleep(60)
+                    try_time += 1
+
+            if RUN_MODE == '1':
+                while job_status in ('RUNNING'):
+                    job_response = requests.request("GET", job_url, headers=longTest.headers)
+                    job_info = json.loads(job_response.text)
+                    job_status = job_info['data']['job_status']
+                    killYarnApplications()
+                self.assertEquals(job_status, 'ERROR', 'Build cube failed, job status is ' + job_status)
+            else:
+                self.assertEquals(job_status, 'FINISHED', 'Build cube failed, job status is ' + job_status)
+            print 'Job complete.'
+            timeend = datetime.now()
+            dtime = (timeend - starttime).seconds
+            sql = '''
+            INSERT INTO `longtest`.`result`(`id`, `name`, `request`, `respons`, `starttime`, `endtime`, `status`, `date`,`D_time`) VALUES (null, '{namepro}', '', '', '{timestart}', '{timeend}', '{status}','{timeend}', '{D_time}');
+            '''
+            sql = sql.format(namepro="testQuery1", request1=str(response.url), respons1=str(response.text),
+                             timestart=starttime, timeend=timeend, D_time=dtime, status=str(response.status_code))
+
 
 
 if __name__ == '__main__':
