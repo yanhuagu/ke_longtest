6eba7f0966f1690c714f5cd227794f1bddbdd013
Yanhua Gu <yanhua.gu@YanhuaGus-MacBook-Pro.local>
2018-08-20 13:07:19 +0800

    a

--- longtest/tests/test_Pushdown_query.py
+++ longtest/tests/test_Pushdown_query.py
@@ -30,7 +30,7 @@ RUN_MODE = 0
 
 @ddt.ddt
 class pushdownTest(unittest.TestCase):
-    base_url = "http://10.1.2.108:7070/kylin/api"
+    base_url = "http://10.1.1.83:7070/kylin/api"
     headers = {
         'content-type': "application/json",
         'authorization': "Basic QURNSU46S1lMSU4=",
@@ -64,28 +64,19 @@ class pushdownTest(unittest.TestCase):
 
     # tpch_kap_24
     @ ddt.data(
-        #####tpch
-               # ['''{"acceptPartial":true,"limit":50000,"offset":0,"project":"tpch_kap_24","sql":"select   c_count,   count(*) as custdist from   (     select       c_custkey,       count(o_orderkey) as c_count     from       customer left outer join orders on         c_custkey = o_custkey         and o_comment not like '%unusual%accounts%'     group by       c_custkey   ) c_orders group by   c_count order by   custdist desc,   c_count desc;","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],#Q13 hive可查
-               # ['''{"acceptPartial":true,"limit":50000,"offset":0,"project":"tpch_kap_24","sql":"with tmp1 as (     select p_partkey from part where p_name like 'forest%' ), tmp2 as (     select s_name, s_address, s_suppkey     from supplier, nation     where s_nationkey = n_nationkey     and n_name = 'CANADA' ), tmp3 as (     select l_partkey, 0.5 * sum(l_quantity) as sum_quantity, l_suppkey     from lineitem, tmp2     where l_shipdate >= '1994-01-01' and l_shipdate <= '1995-01-01'     and l_suppkey = s_suppkey      group by l_partkey, l_suppkey ), tmp4 as (     select ps_partkey, ps_suppkey, ps_availqty     from partsupp      where ps_partkey IN (select p_partkey from tmp1) ), tmp5 as ( select     ps_suppkey from     tmp4, tmp3 where     ps_partkey = l_partkey     and ps_suppkey = l_suppkey     and ps_availqty > sum_quantity ) select     s_name,     s_address from     supplier where     s_suppkey IN (select ps_suppkey from tmp5) order by s_name;","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],# Q20
 
-        [
-            '''{"acceptPartial":true,"limit":"5","offset":0,"project":"learn_kylin","sql":"select lstg_format_name, sum(price) as GMV from kylin_sales where lstg_format_name='FP-GTC' group by lstg_format_name","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
-        [
-            '''{"acceptPartial":true,"limit":"5","offset":0,"project":"learn_kylin","sql":"select sum(price) as GMV, count(1) as TRANS_CNT from kylin_sales","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
-        [
-            '''{"acceptPartial":true,"limit":"5","offset":0,"project":"learn_kylin","sql":"select kylin_sales.lstg_format_name, sum(price) as GMV, count(*) as TRANS_CNT from kylin_sales group by kylin_sales.lstg_format_name;","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
-        [
-            '''{"acceptPartial":true,"limit":"5","offset":0,"project":"learn_kylin","sql":"select kylin_sales.lstg_format_name,sum(kylin_sales.price) as GMV, count(*) as TRANS_CNT from kylin_sales group by kylin_sales.lstg_format_name having sum(price)>5000","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
-        # [
-        #     '''{"acceptPartial":true,"limit":"5","offset":0,"project":"learn_kylin","sql":"select kylin_sales.lstg_format_name,sum(kylin_sales.price) as GMV, count(*) as TRANS_CNT from kylin_sales where kylin_sales.lstg_format_name is null group by kylin_sales.lstg_format_name having sum(price)>5000 and count(*)>72 ","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
-        # [
-        #     '''{"acceptPartial":true,"limit":"5","offset":0,"project":"learn_kylin","sql":"select kylin_cal_dt.week_beg_dt,sum(kylin_sales.price) as GMV  , count(*) as TRANS_CNT , sum(kylin_sales.item_count) as total_items from kylin_sales  inner JOIN kylin_cal_dt ON kylin_sales.part_dt = kylin_cal_dt.cal_dt inner JOIN kylin_category_groupings ON kylin_sales.leaf_categ_id = kylin_category_groupings.leaf_categ_id AND kylin_sales.lstg_site_id = kylin_category_groupings.site_id where kylin_sales.lstg_format_name='FP-GTC'  and kylin_cal_dt.week_beg_dt between DATE '2013-05-01' and DATE '2013-08-01' group by kylin_cal_dt.week_beg_dt;","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
-        # [
-        #     '''{"acceptPartial":true,"limit":"5","offset":0,"project":"learn_kylin","sql":"SELECT  kylin_cal_dt.week_beg_dt  ,kylin_category_groupings.meta_categ_name  ,sum(price) as GMV, count(*) as TRANS_CNT , sum(kylin_sales.item_count) as total_items FROM kylin_sales  inner JOIN kylin_cal_dt ON kylin_sales.part_dt = kylin_cal_dt.cal_dt inner JOIN kylin_category_groupings ON kylin_sales.leaf_categ_id = kylin_category_groupings.leaf_categ_id AND kylin_sales.lstg_site_id = kylin_category_groupings.site_id where kylin_cal_dt.week_beg_dt between DATE '2013-09-01' and DATE '2013-10-01'  and kylin_category_groupings.categ_lvl3_name='Other'  group by kylin_cal_dt.week_beg_dt  ,kylin_category_groupings.meta_categ_name;","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
-        # [
-        #     '''{"acceptPartial":true,"limit":"5","offset":0,"project":"learn_kylin","sql":"select count(*) as cnt from kylin_sales where lstg_format_name>='AAAA' and 'BBBB'>=lstg_format_name;","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
-        # [
-        #     '''{"acceptPartial":true,"limit":"5","offset":0,"project":"learn_kylin","sql":"select kylin_sales.lstg_format_name, sum(price) as GMV, count(seller_id) as TRANS_CNT  from kylin_sales where kylin_sales.lstg_format_name > 'AB'  group by kylin_sales.lstg_format_name having count(seller_id) > 2;","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
+        ###ssb
+        ['''{"acceptPartial":true,"limit":"5","offset":0,"project":"ssb","sql":"select sum(v_revenue) as revenue  from p_lineorder  left join dates on lo_orderdate = d_datekey  where d_year = 1993  and lo_discount between 1 and 3  and lo_quantity < 25;","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
+        ['''{"acceptPartial":true,"limit":"5","offset":0,"project":"ssb","sql":"select sum(v_revenue) as revenue from p_lineorder left join dates on lo_orderdate = d_datekey where d_yearmonthnum = 199401 and lo_discount between 4 and 6 and lo_quantity between 26 and 35;","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
+        ['''{"acceptPartial":true,"limit":"5","offset":0,"project":"ssb","sql":"select sum(v_revenue) as revenue from p_lineorder left join dates on lo_orderdate = d_datekey where d_weeknuminyear = 6 and d_year = 1994 and lo_discount between 5 and 7 and lo_quantity between 26 and 35;","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}''']
+
+
+
+
+        # #####tpch
+        #        # ['''{"acceptPartial":true,"limit":50000,"offset":0,"project":"tpch_kap_24","sql":"select   c_count,   count(*) as custdist from   (     select       c_custkey,       count(o_orderkey) as c_count     from       customer left outer join orders on         c_custkey = o_custkey         and o_comment not like '%unusual%accounts%'     group by       c_custkey   ) c_orders group by   c_count order by   custdist desc,   c_count desc;","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],#Q13 hive可查
+        #        # ['''{"acceptPartial":true,"limit":50000,"offset":0,"project":"tpch_kap_24","sql":"with tmp1 as (     select p_partkey from part where p_name like 'forest%' ), tmp2 as (     select s_name, s_address, s_suppkey     from supplier, nation     where s_nationkey = n_nationkey     and n_name = 'CANADA' ), tmp3 as (     select l_partkey, 0.5 * sum(l_quantity) as sum_quantity, l_suppkey     from lineitem, tmp2     where l_shipdate >= '1994-01-01' and l_shipdate <= '1995-01-01'     and l_suppkey = s_suppkey      group by l_partkey, l_suppkey ), tmp4 as (     select ps_partkey, ps_suppkey, ps_availqty     from partsupp      where ps_partkey IN (select p_partkey from tmp1) ), tmp5 as ( select     ps_suppkey from     tmp4, tmp3 where     ps_partkey = l_partkey     and ps_suppkey = l_suppkey     and ps_availqty > sum_quantity ) select     s_name,     s_address from     supplier where     s_suppkey IN (select ps_suppkey from tmp5) order by s_name;","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],# Q20
+        #
         # [
         #     '''{"acceptPartial":true,"limit":"5","offset":0,"project":"learn_kylin","sql":"select lstg_format_name, sum(price) as GMV from kylin_sales where lstg_format_name='FP-GTC' group by lstg_format_name","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
         # [
@@ -94,16 +85,8 @@ class pushdownTest(unittest.TestCase):
         #     '''{"acceptPartial":true,"limit":"5","offset":0,"project":"learn_kylin","sql":"select kylin_sales.lstg_format_name, sum(price) as GMV, count(*) as TRANS_CNT from kylin_sales group by kylin_sales.lstg_format_name;","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
         # [
         #     '''{"acceptPartial":true,"limit":"5","offset":0,"project":"learn_kylin","sql":"select kylin_sales.lstg_format_name,sum(kylin_sales.price) as GMV, count(*) as TRANS_CNT from kylin_sales group by kylin_sales.lstg_format_name having sum(price)>5000","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
-        # [
-        #     '''{"acceptPartial":true,"limit":"5","offset":0,"project":"learn_kylin","sql":"select kylin_sales.lstg_format_name,sum(kylin_sales.price) as GMV, count(*) as TRANS_CNT from kylin_sales where kylin_sales.lstg_format_name is null group by kylin_sales.lstg_format_name having sum(price)>5000 and count(*)>72 ","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
-        # [
-        #     '''{"acceptPartial":true,"limit":"5","offset":0,"project":"learn_kylin","sql":"select kylin_cal_dt.week_beg_dt,sum(kylin_sales.price) as GMV  , count(*) as TRANS_CNT , sum(kylin_sales.item_count) as total_items from kylin_sales  inner JOIN kylin_cal_dt ON kylin_sales.part_dt = kylin_cal_dt.cal_dt inner JOIN kylin_category_groupings ON kylin_sales.leaf_categ_id = kylin_category_groupings.leaf_categ_id AND kylin_sales.lstg_site_id = kylin_category_groupings.site_id where kylin_sales.lstg_format_name='FP-GTC'  and kylin_cal_dt.week_beg_dt between DATE '2013-05-01' and DATE '2013-08-01' group by kylin_cal_dt.week_beg_dt;","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
-        # [
-        #     '''{"acceptPartial":true,"limit":"5","offset":0,"project":"learn_kylin","sql":"SELECT  kylin_cal_dt.week_beg_dt  ,kylin_category_groupings.meta_categ_name  ,sum(price) as GMV, count(*) as TRANS_CNT , sum(kylin_sales.item_count) as total_items FROM kylin_sales  inner JOIN kylin_cal_dt ON kylin_sales.part_dt = kylin_cal_dt.cal_dt inner JOIN kylin_category_groupings ON kylin_sales.leaf_categ_id = kylin_category_groupings.leaf_categ_id AND kylin_sales.lstg_site_id = kylin_category_groupings.site_id where kylin_cal_dt.week_beg_dt between DATE '2013-09-01' and DATE '2013-10-01'  and kylin_category_groupings.categ_lvl3_name='Other'  group by kylin_cal_dt.week_beg_dt  ,kylin_category_groupings.meta_categ_name;","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
-        # [
-        #     '''{"acceptPartial":true,"limit":"5","offset":0,"project":"learn_kylin","sql":"select count(*) as cnt from kylin_sales where lstg_format_name>='AAAA' and 'BBBB'>=lstg_format_name;","backdoorToggles":{"DEBUG_TOGGLE_HTRACE_ENABLED":false}}'''],
 
-    )
+     )
     @ ddt.unpack
     @BeautifulReport.add_test_img('bbbb')
 
